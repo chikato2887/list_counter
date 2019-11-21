f573b9948fb59847f04c4c6fe32322f35bab2b64
chikato2887 <suganuma.matsumoto03@gmail.com>
2019-11-22 02:14:31 +0900

    add: most_common function

--- counter.ts
+++ counter.ts
@@ -32,6 +32,19 @@ export class ListCounter <T, K extends keyof T> {
     return aggregates;
   }
 
+  mostCommon(by: K | null = null): {[key: string]: number}[] {
+    const aggregated = this.aggregate(by);
+    const aggregateList = Object.keys(aggregated).map(key => ({[key]: aggregated[key]}));
+    const commonList = aggregateList.sort((prev, next) => {
+      const prevKey = Object.keys(prev)[0];
+      const nextKey = Object.keys(next)[0];
+      if(prev[prevKey] < next[nextKey]) return 1;
+      else if(prev[prevKey] > next[nextKey]) return -1;
+      else return 0;
+    });
+    return commonList;
+  }
+
   comparator(prev: T, next: T, by: K | null) {
     if(by === null) return 0;
     else if(prev[by] < next[by]) return -1;
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { ListCounter } from './counter';
 const referenceList: User[] = [
   new User({id: 1, name: "mike", email: "sample1@example.com", age: 20}),
   new User({id: 3, name: "nancy", email: "sample3@example.com", age: 21}),
-  new User({id: 2, name: "john", email: "sample2@example.com", age: 20}), 
+  new User({id: 2, name: "mike", email: "sample2@example.com", age: 20}), 
 ];
 
 const primitiveList = [1, 4, 3, 5, 4];
@@ -15,5 +15,8 @@ const primitiveCounter = new ListCounter(primitiveList);
 // console.log("reference: ", referenceCounter.sort("id"));
 // console.log("premitive: ", primitiveCounter.sort());
 
-// console.log("referenceCounter: ", referenceCounter.aggregate("age"));
-// console.log("primitiveCounter: ", primitiveCounter.aggregate());
+console.log("referenceCounter: ", referenceCounter.aggregate("name"));
+console.log("primitiveCounter: ", primitiveCounter.aggregate());
+
+console.log("most premitive: ", referenceCounter.mostCommon("name"));
+console.log("primitive premitive: ", primitiveCounter.mostCommon());
